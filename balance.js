b4b354530b8f36f6f76757bc29e3c6f648c5c43c
Alex Quang <alexquang1991@gmail.com>
2016-04-30 16:09:08 -0700

    modified balance point algorithm

--- balance.js
+++ balance.js
@@ -1,4 +1,4 @@
-function balance(arr) {
+function balanceOld(arr) {
   if (arr.length < 3) {
     return false;
   }
@@ -25,3 +25,32 @@ function balance(arr) {
   
   return false;
 }
+
+// New balance point algorithm that is better than the old one
+function balance(arr) {
+  if (arr.length < 3) {
+    return false;
+  }
+  
+  var midIdx = Math.floor(arr.length/2),
+      left = 0,
+      right = 0;
+  
+  for (var i = 0; i < arr.length; i++) {
+    if (i < midIdx)
+    {
+      left += arr[i];
+    }
+    else if (i > midIdx) {
+      right += arr[i];
+    }
+  }
+  
+  if (left == right) {
+    return true;
+  }
+  
+  return false;
+}
+
+console.log(balance([1, 2, 3, 4, 1, 2, 3]));
