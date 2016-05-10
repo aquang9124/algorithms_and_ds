ed0f38aadf7787a084a40c71658e949d7797f5fc
Alex Quang <alexquang1991@gmail.com>
2016-05-10 16:31:23 -0700

    minor progress on being able to merge sort a linked list

--- mergesingly.js
+++ mergesingly.js
@@ -12,6 +12,10 @@ sll.insert(2, 7);
 sll.insert(4, 2);
 
 function mergeSortLinkedList(sll) {
+	if (sll.nodeCount < 2) {
+		return sll;
+	}
+
 	var left = new singlyLinkedList(),
 		right = new singlyLinkedList(),
 		current = sll.head.next,
@@ -20,13 +24,32 @@ function mergeSortLinkedList(sll) {
 		rightCurr = right.head;
 
 	while (current !== null) {
-		if (pivot.data < current.data) {
+		if (pivot.data > current.data) {
 			if (left.head === null) {
 				left.head = new Node(current.data);
+				current.next = current.next.next;
+				leftCurr = left.head;
 			} else {
-				console.log(leftCurr);
 				leftCurr.next = new Node(current.data);
 				leftCurr = leftCurr.next;
+				if (current.next !== null) {
+					current.next = current.next.next;
+				}
+				
+			}
+		}
+
+		if (pivot.data < current.data) {
+			if (right.head === null) {
+				right.head = new Node(current.data);
+				current.next = current.next.next;
+				rightCurr = right.head;
+			} else {
+				rightCurr.next = new Node(current.data);
+				rightCurr = rightCurr.next;
+				if (current.next !== null) {
+					current.next = current.next.next;
+				}
 			}
 		}
 		
@@ -40,4 +63,5 @@ function mergeLinkedLists(left, right) {
 	return false;
 }
 
-console.log(mergeSortLinkedList(sll));
+var answer = mergeSortLinkedList(sll);
+answer.display();
