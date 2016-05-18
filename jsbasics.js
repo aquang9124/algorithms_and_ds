02236a81339af69db499bf2a5f4ace21db66bbc6
Alex Quang <alexquang1991@gmail.com>
2016-05-18 16:56:51 -0700

    finished another algo

--- jsbasics.js
+++ jsbasics.js
@@ -55,3 +55,57 @@ function rFib(n) {
 
 	return rFib(n - 1) + rFib(n - 2);
 }
+
+// find greatest common divisor
+// Only works if a is lower than b
+function greatestCommonDivisor(a, b) {
+	if (b === 0) {
+		return a;
+	} else {
+		return greatestCommonDivisor(a, a%b);
+	}
+}
+
+// Remove duplicates from an array
+function removeDupes(arr) {
+	var exists = {},
+		arr2 = [];
+
+	for (var i = 0; i < arr.length; i++) {
+		if (exists[arr[i]] !== true) {
+			arr2.push(arr[i]);
+		}
+		exists[arr[i]] = true;
+	}
+
+	return arr2;
+}
+
+// Stat doubles function
+function statDoubles() {
+	var die = Math.floor(Math.random() * 6) + 1,
+		die2 = Math.floor(Math.random() * 6) + 1,
+		max = die + die2,
+		min = die + die2,
+		avg = die + die2,
+		numRolls = 1;
+
+	while (die != die2) {
+		if (max < (die + die2)) {
+			max = die + die2;
+		}
+
+		if (min > (die + die2)) {
+			min = die + die2;
+		}
+
+		numRolls++;
+		avg += die;
+		avg += die2;
+		die = Math.floor(Math.random() * 6) + 1;
+		die2 = Math.floor(Math.random() * 6) + 1;
+	}
+
+	avg = Math.floor(avg / numRolls);
+	return [numRolls, max, min, avg];
+}
