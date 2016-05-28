8ec67bbcdc332991d9994a626e96c8f5e75335d1
Alex Quang <alexquang1991@gmail.com>
2016-05-28 14:38:27 -0700

    refined movezeroes

--- leeteasy.js
+++ leeteasy.js
@@ -20,8 +20,7 @@ function intersection(nums1, nums2) {
 
 // move zeroes to the end
 var moveZeroes = function(nums) {
-
-    for (var i = 0; i < nums.length; i++) {
+	for (var i = 0; i < nums.length; i++) {
         if (nums[i] === 0)
         {
             for (var j = i; j < nums.length - 1; j++) {
@@ -29,24 +28,14 @@ var moveZeroes = function(nums) {
                 nums[j] = nums[j + 1];
                 nums[j + 1] = temp;
             }
+            
+            if (nums[i] !== 0) {
+   				break;
+            }
+
+            i--;
         }
     }
 
-    // Second sweep to make sure all zeroes are accounted for
-    // Not sure why it wouldn't just let me decrement i
-    // when I found a zero.
-    for (var idx = 0; idx < nums.length; idx++) {
-    	if (nums[idx] === 0)
-        {
-            for (var k = idx; k < nums.length - 1; k++) {
-                var temp2 = nums[k];
-                nums[k] = nums[k + 1];
-                nums[k + 1] = temp;
-            }
-        }
-    }
-    
     return nums;
 };
-
-console.log(moveZeroes([0, 0, 0, 1]));
