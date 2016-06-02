6679f19eac4808d921e60c42efe7699aca5ed5fb
Alex Quang <alexquang1991@gmail.com>
2016-06-02 14:22:47 -0700

    added new algos

--- weekone.js
+++ weekone.js
@@ -16,4 +16,22 @@ function findPair(arr, x) {
 	return false;
 }
 
-console.log(findPair([1, 2, 3, 4], 5));
+// iife test
+( function() {
+	var a = b = 5;
+		
+} )();
+// This console logs 5 if you console.log(b) because
+// b is not declared with the var keyword and is thus
+// a global variable
+
+String.prototype.repeatify = String.prototype.repeatify || function(x) {
+	var str = "";
+	while (x > 0) {
+		str += this;
+		x--;
+	}
+
+	return str;
+};
+// Call repeatify on a string to return that string repeated x number of times
