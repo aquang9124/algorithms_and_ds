96abe17ff2227562580b1033888eb8c8d4bfcdb4
Alex Quang <alexquang1991@gmail.com>
2016-06-05 21:19:20 -0700

    added stuff

--- elojs.js
+++ elojs.js
@@ -29,3 +29,28 @@ console.log(Array.prototype.toString.call([1, 2, 3]));
 	console.log(noProto);
 		
 } )();
+
+// testing getters and setters
+var pile = {
+	elements: ['apples', 'oranges', 'bananas'],
+	get height() {
+		return this.elements.length;
+	},
+	set height(value) {
+		console.log("Ignoring attempt to set height to " + value);
+	}
+};
+
+// console.log(pile.height);
+// pile.height = 100;
+
+// testing of object inheritance
+function tableCell(text) {
+	this.text = text.split("\n");
+}
+
+tableCell.prototype.minWidth = function() {
+	return this.text.reduce(function(width, line) {
+		return Math.max(width, line.length);
+	}, 0);
+};
