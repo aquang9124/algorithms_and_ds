db3d3d8d619da1befcd8d578c9e7dcbdbe667ea8
Alex Quang <alexquang1991@gmail.com>
2016-06-12 14:53:08 -0700

    new algos

--- advanced.js
+++ advanced.js
@@ -89,3 +89,104 @@ function splitPro(separator, str) {
 	return arr;
 
 }
+
+// reverse string
+var reverseString = function(s) {
+    var left = 0,
+        right = s.length - 1;
+    str = s.split('');
+    
+    while (left <= right) {
+        var temp = str[left];
+        str[left] = str[right];
+        str[right] = temp;
+        
+        left++;
+        right--;
+    }
+    
+    return str.join('');
+};
+
+// remove negatives
+function removeNegativity(arr) {
+	var numNegatives = 0;
+
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] >= 0) {
+			arr[i - numNegatives] = arr[i];
+		} else {
+			numNegatives++;
+		}
+	}
+
+	arr.length -= numNegatives;
+
+	return arr;
+}
+
+// merge sort
+var mergeSort = function(arr) {
+	var left = arr.slice(0, Math.floor(arr.length / 2));
+	var right = arr.slice(Math.floor(arr.length / 2));
+
+	if (arr.length < 2) {
+		return arr;
+	}
+
+	return mergeArrays(mergeSort(left), mergeSort(right));
+}
+
+var mergeArrays = function(arr1, arr2) {
+	var result = [];
+
+	while (arr1.length && arr2.length) {
+		if (arr2[0] >= arr1[0]) {
+			result.push(arr1.shift());
+		}
+		else if (arr1[0] > arr2[0]) {
+			result.push(arr2.shift());
+		}
+	}
+
+	while (arr1.length)
+		result.push(arr1.shift());
+
+	while (arr2.length)
+		result.push(arr2.shift());
+
+	return result;
+};
+
+// quick sort
+Array.prototype.quickSwap = function(idx1, idx2) {
+	var temp = this[idx1];
+	this[idx1] = this[idx2];
+	this[idx2] = temp;
+
+	return this;
+};
+
+var quickSort = function(arr) {
+
+	var placePivot = function(start, end) {
+		if (start >= end) {
+			return;
+		}
+
+		var mid = start;
+		for (var i = start; i < end; i++) {
+			if (arr[i] < arr[end]) {
+				arr.quickSwap(mid, i);
+				mid++;
+			}
+		}
+
+		arr.quickSwap(mid, end);
+		placePivot(start, mid - 1);
+		placePivot(mid + 1, end);
+	}
+
+	placePivot(0, arr.length - 1);
+	return arr;
+}
