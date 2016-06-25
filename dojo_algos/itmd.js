b6d9ce46100ead3fa07a4fc677d375f72c3b7015
Alex Quang <alexquang1991@gmail.com>
2016-06-24 19:31:53 -0700

    new algos

--- dojo_algos/itmd.js
+++ dojo_algos/itmd.js
@@ -128,3 +128,184 @@ function rBSearch(arr, target, mid, min, max) {
 	mid = Math.floor((max + min) / 2);
 	return rBSearch(arr, target, mid, min, max);
 }
+
+// recursively flatten an array of sub arrays
+function flatten(arr) {
+	var result = [];
+
+	function makeFlat(element) {
+		if (!Array.isArray(element)) {
+			result.push(element);
+		}
+		else {
+			for (var i = 0; i < element.length; i++) {
+				makeFlat(element[i]);
+			}
+		}
+	}
+
+	makeFlat(arr);
+	return result;
+}
+
+// [1, 2, 3, 4], 3 = [[1, 2, 3], [4]]
+function randomFunc(arr, num) {
+	if (num <= 0) {
+		return [];
+	}
+
+	var result = [];
+	var tempArr = [];
+
+	for (var i = 0; i < arr.length; i++) {
+		if (tempArr.length === num) {
+			result.push(tempArr);
+			tempArr = [];
+		}
+		tempArr.push(arr[i]);
+	}
+
+	if (tempArr.length > 0) {
+		result.push(tempArr);
+	}
+
+	return result;
+}
+
+// loop through arguments and print each one
+function argumentLoop(...args) {
+	if (Array.isArray(args[0])) {
+		argumentLoop.apply(null, args[0]);
+		return;
+	}
+	for (var i = 0; i < args.length; i++) {
+		console.log(args[i]);
+	}
+}
+
+// get all variations of string
+function getSets(str) {
+	var results = [];
+
+	function makeSets(char, depth) {
+		if (depth === str.length) {
+			results.push(char);
+			return;
+		}
+		// left side
+		makeSets(char, depth + 1);
+		// right side
+		makeSets(char + str[depth], depth + 1);
+	}
+
+	makeSets('', 0);
+
+	return results;
+}
+
+// happy number
+function isHappy(num) {
+	
+	while (Math.pow(num, 2) >= 10) {
+		var rem = Math.floor(num / 10);
+		if (rem < 10) {
+			num = Math.pow(rem, 2) + Math.pow(num % 10, 2);
+		}
+		else {
+			num = rem + Math.pow(num % 10, 2);
+		}
+	}
+
+	if (num === 1) {
+		return true;
+	}
+
+	return false;
+}
+
+// multiply by 3, 5, or add 1 to reach target number
+function getToNum(num) {
+	var count = 0;
+
+	function makeCalculations(start) {
+		if (start === num) {
+			count++;
+			return;
+		}
+		else if (start > num) {
+			return;
+		}
+		// add one
+		makeCalculations(start + 1);
+		// multiply by 3
+		makeCalculations(start * 3);
+		// multiply by 5
+		makeCalculations(start * 5);
+	}
+
+	makeCalculations(1);
+	return count;
+}
+
+// Different implementation using pure recursion
+function getToTarget(num, rootNum) {
+	rootNum = rootNum || 1;
+
+	if (rootNum === num) {
+		return 1;
+	}
+	else if (rootNum > num) {
+		return 0;
+	}
+
+	return getToTarget(num, rootNum + 1) + getToTarget(num, rootNum * 3) + getToTarget(num, rootNum * 5);
+}
+
+// linked list stuff
+function ListNode(value) {
+	this.value = value;
+	this.next = null;
+}
+
+function SinglyLinkedList() {
+	this.head = this.tail = null;
+}
+
+SinglyLinkedList.prototype = {
+	insert: function(value) {
+		var newNode = new ListNode(value);
+
+		if (this.head === null) {
+			this.head = newNode;
+			this.tail = newNode;
+		}
+		else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+
+		return this;
+	},
+	average: function() {
+		if (this.head === null) {
+			return 0;
+		}
+		else {
+			var count = 0,
+				average = this.head.value,
+				current = this.head;
+
+			while (current) {
+				count++;
+				average += current.value;
+				current = current.next;
+			}
+			average /= count;
+			return average;
+		}
+	}
+};
+
+var sll = new SinglyLinkedList();
+sll.insert(6).insert(7).insert(8);
+console.log(sll);
