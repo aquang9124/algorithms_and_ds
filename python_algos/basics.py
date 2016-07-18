ed5e3dfc56f5ab85f95bf0730b43725bc5952854
Alex Quang <alexquang1991@gmail.com>
2016-07-17 20:39:14 -0700

    more python algos

--- python_algos/basics.py
+++ python_algos/basics.py
@@ -1,6 +1,4 @@
-# python json parsing
-import json
-
+# python json fun
 fruits = [ 
 { "name" : "apples", 
 "baskets" : [10,20,30] 
@@ -10,12 +8,37 @@ fruits = [
 } 
 ]
 
-for x in range(0, len(fruits)):
-	baskets_sum = 0
-	print(fruits[x]['name'], end=" ")
-	print(len(fruits[x]['baskets']), end=" ")
+def print_fruits():
+	for x in range(0, len(fruits)):
+		baskets_sum = 0
+		print(fruits[x]['name'], end=" ")
+		print(len(fruits[x]['baskets']), end=" ")
 
-	for y in range(0, len(fruits[x]['baskets'])):
-		baskets_sum += fruits[x]['baskets'][y]
+		for y in range(0, len(fruits[x]['baskets'])):
+			baskets_sum += fruits[x]['baskets'][y]
 
-	print(baskets_sum)
+		print(baskets_sum)
+
+def sum67(nums):
+	result = 0
+	not_in_deadzone = True
+
+	for idx in range(0, len(nums)):
+		if nums[idx] == 6:
+			not_in_deadzone = False
+
+		if not_in_deadzone == True:
+			result += nums[idx]
+
+		if nums[idx] == 7:
+			not_in_deadzone = True
+
+	return result
+
+def has_adjacent_2(nums):
+
+	for idx in range(0, len(nums)):
+		if nums[idx] == 2 and nums[idx + 1] == 2:
+			return True
+
+	return False
