0dd9f31ad536f6cda04eed0891012e6b7e565e0c
Alex Quang <alexquang1991@gmail.com>
2016-07-18 22:49:31 -0700

    new python

--- python_algos/basics.py
+++ python_algos/basics.py
@@ -298,12 +298,19 @@ class BinaryGate(LogicGate):
 		self.pin_b = None
 
 	def get_pin_a(self):
-		self.pin_a = int(input("Enter Pin A input for gate %s -->" % self.get_label()))
-		return self.pin_a
+		return int(input("Enter Pin A input for gate %s --> " % self.get_label()))
 
 	def get_pin_b(self):
-		self.pin_b = int(input("Enter Pin B input for gate %s -->" % self.get_label()))
-		return self.pin_b
+		return int(input("Enter Pin B input for gate %s --> " % self.get_label()))
+
+	def set_next_pin(self, source):
+		if self.pin_a == None:
+			self.pin_a = source
+		else:
+			if self.pin_b == None:
+				self.pin_b = source
+			else:
+				raise RunTimeError("Error: NO EMPTY PINS")
 
 class UnaryGate(LogicGate):
 	def __init__(self, n):
@@ -312,14 +319,14 @@ class UnaryGate(LogicGate):
 		self.pin = None
 
 	def get_pin(self):
-		return int(input("Enter Pin input for gate %s" % self.get_label()))
+		return int(input("Enter Pin input for gate %s --> " % self.get_label()))
 
 class AndGate(BinaryGate):
 	def __init__(self, n):
 		super().__init__(n)
 
 	def __str__(self):
-		return 'Gate %s has Pin A input of %s and Pin B input of %s' % (self.get_label(), str(self.pin_a), str(self.pin_b))
+		return 'Gate %s' % self.get_label()
 
 	def perform_gate_logic(self):
 		a = self.get_pin_a()
@@ -346,6 +353,34 @@ class OrGate(BinaryGate):
 		else:
 			return 0
 
-g1 = OrGate('G1')
+class NotGate(UnaryGate):
+	def __init__(self, n):
+		super().__init__(n)
+
+	def __str__(self):
+		return self.get_label()
+
+	def perform_gate_logic(self):
+		pin = self.get_pin()
+
+		if pin == 1:
+			return 1
+		else:
+			return 0
+
+class Connector:
+	def __init__(self, fgate, tgate):
+		self.from_gate = fgate
+		self.to_gate = tgate
+
+		tgate.set_next_pin(self)
+
+	def get_from(self):
+		return self.from_gate
+
+	def get_to(self):
+		return self.to_gate
+
+g1 = AndGate('G1')
 g1.get_output()
 print(g1)
