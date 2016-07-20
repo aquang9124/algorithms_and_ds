69426dd71b0ae9b5031e7b7590bfc7bbd07b9896
Alex Quang <alexquang1991@gmail.com>
2016-07-20 14:15:05 -0700

    python algos

--- python_algos/itmd.py
+++ python_algos/itmd.py
@@ -32,3 +32,52 @@ def is_ana_palindrome(anagram):
 		return False
 	else:
 		return True
+
+# string encode
+def string_encode(string):
+	result_string = ""
+	i = 0
+
+	while i < len(string):
+		idx = i
+		char = string[i]
+		count = 0
+		still_ok = True
+
+		while idx < len(string) and still_ok:
+			if string[idx] == char:
+				count += 1
+				idx += 1
+			else:
+				still_ok = False
+
+		result_string += "%s%s" % (char, str(count))
+		i = idx
+
+	return result_string
+
+# shorten string length
+def shorten_str(string, target_len):
+	length = len(string)
+	str_list = list(string)
+
+	while length != target_len:
+		i = 0
+		too_long = True
+
+		while i < len(str_list) and too_long:
+			length = len(str_list)
+
+			if length == target_len:
+				too_long = False
+			elif str_list[i] == " ":
+				front = str_list[0:i]
+				mid = i + 1
+				rest = str_list[mid:]
+				str_list = front + rest
+				
+			i += 1
+
+	return ''.join(str_list)
+
+print(shorten_str("It's a wo nd erful life! ", 20))
