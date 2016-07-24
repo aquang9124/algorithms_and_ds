de6b66d401375c6844167490ff2d84a86cabd0f1
Alex Quang <alexquang1991@gmail.com>
2016-07-24 16:43:27 -0700

    js algos

--- dojo_algos/special.js
+++ dojo_algos/special.js
@@ -90,3 +90,34 @@ class Queue {
 		return this.items.pop();
 	}
 }
+
+// count 8 recursively, double the value if the next value is an 8
+function countEight(n) {
+	let count = 0;
+
+	function countIt(num) {
+		cNum = num % 10;
+		rem = Math.floor(num / 10);
+		leftOfCNum = rem % 10;
+
+		if (num < 10) {
+			if (num === 8) {
+				count++;
+			}
+			return;
+		}
+
+		if (leftOfCNum === 8 && cNum === 8) {
+			count += 2;
+		}
+		else if (cNum === 8) {
+			count++;
+		}
+
+		countIt(Math.floor(num / 10));
+	}
+
+	countIt(n);
+	return count;
+}
+
