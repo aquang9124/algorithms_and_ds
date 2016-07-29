ce097f3c8715e86c185f95b9c6ba92e2fa8fe514
Alex Quang <alexquang1991@gmail.com>
2016-07-29 15:01:46 -0700

    codewars

--- ds_and_algos/basics.js
+++ ds_and_algos/basics.js
@@ -333,3 +333,29 @@ function cleverSum(n) {
 }
 
 // jaden casing
+String.prototype.toJadenCase = function() {
+	if (this.trim() === "") {
+		return false;
+	}
+
+	let s = this.split(' ');
+
+	for (let i = 0; i < s.length; i++) {
+		let word = s[i].split('');
+		word[0] = word[0].toUpperCase();
+		s[i] = word.join('');
+	}
+
+	return s.join(' ');
+};
+
+// more functional implementation of jaden casing
+String.prototype.toJaden = function() {
+	let s = this.split(' ');
+
+	s = s.map(function(word) {
+		return word.charAt(0).toUpperCase() + word.slice(1);
+	});
+
+	return s.join(' ');
+};
