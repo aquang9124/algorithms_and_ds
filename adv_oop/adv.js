e611ab903b648c9026b01d3adda660cb7e17ae8b
Alex Quang <alexquang1991@gmail.com>
2016-08-04 14:29:52 -0700

    bst fun

--- adv_oop/adv.js
+++ adv_oop/adv.js
@@ -62,12 +62,91 @@ function Person(_universe, name) {
 
     this.shout = wrapExposedMethod( (phrase = "I like eggs") => {
         let result = `${this.name} shouts out: ${phrase}!`;
-
+        console.log(result);
         return result;
     } );
 }
 
-let uni = new Universe();
-let dude = new Person(uni, 'Don');
+// fun with binary trees
+class BTNode {
+    constructor(data) {
+        this.data = data;
+        this.l = null;
+        this.r = null;
+    }
+}
 
-console.log(dude.shout('Oooh sausages'));
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    add(data) {
+        let newNode = new BTNode(data);
+
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            let cNode = this.root;
+
+            while (true) {
+                if (cNode.data >= data) {
+                    if (cNode.l === null) {
+                        cNode.l = newNode;
+                        break;
+                    } else {
+                        cNode = cNode.l;
+                    }
+                } else {
+                    if (cNode.r === null) {
+                        cNode.r = newNode;
+                        break;
+                    } else {
+                        cNode = cNode.r;
+                    }
+                }
+            }
+        }
+
+        return this;
+    }
+
+    contains(data) {
+        if (this.root === null) {
+            return -1;
+        }
+        else if (this.root.data === data) {
+            return true;
+        }
+        else {
+            let cNode = this.root;
+
+            while (cNode) {
+                if (cNode.l !== null && data <= cNode.data) {
+                    cNode = cNode.l;
+
+                    if (cNode.data === data) {
+                        return true;
+                    }
+                } else if (cNode.r !== null) {
+                    cNode = cNode.r;
+
+                    if (cNode.data === data) {
+                        return true;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
+
+let bst = new BinarySearchTree();
+bst.add(2).add(6).add(4);
+console.log(bst.contains(6));
+
+let logApples = (n) => console.log(`${n} apples!`);
+logApples(3);
