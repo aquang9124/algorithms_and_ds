0e4bf2fcfc6e4f0177192ae8c950d9ca757cf254
Alex Quang <alexquang1991@gmail.com>
2016-08-05 12:11:07 -0700

    python

--- codewars/pykata.py
+++ codewars/pykata.py
@@ -1,3 +1,4 @@
+import random
 # powers of two
 def powers_of_two(n):
 	return [2**i for i in range(n + 1)]
@@ -19,3 +20,20 @@ def xo(s):
 			o += 1
 
 	return x == o
+
+def generate_arr(n):
+	return [random.randrange(1, 200) for i in range(n)]
+
+def bub_sort(arr):
+	swapped = True
+	limit = 1
+
+	while swapped:
+		swapped = False
+
+		for i in range(len(arr) - limit):
+			if arr[i] > arr[i + 1]:
+				arr[i], arr[i + 1] = arr[i + 1], arr[i]
+				swapped = True
+
+	return arr
