283dd7a40a9896e1c55b2e39e6f0416b7e6600b8
Alex Quang <alexquang1991@gmail.com>
2016-08-04 18:08:49 -0700

    python

--- codewars/pykata.py
+++ codewars/pykata.py
@@ -2,4 +2,11 @@
 def powers_of_two(n):
 	return [2**i for i in range(n + 1)]
 
-print(powers_of_two(1))
+def add_binary(a,b):
+    #your code here
+    sum = a + b
+    return bin(sum)[2:]
+
+def xo(s):
+	s = s.lower()
+	return s.count('x') == s.count('o')
