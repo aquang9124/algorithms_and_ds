c8387d98a87e161ef811e432a1edd4a2d63c2c5d
Alex Quang <alexquang1991@gmail.com>
2016-08-10 14:15:03 -0700

    got api key

--- expe_calc/server/server.js
+++ expe_calc/server/server.js
@@ -2,20 +2,21 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var http = require('http');
+var bartKey = require('../config.json').key;
 var app = express();
 
 // set port
-app.set('port', (process.env.PORT || 8000));
+app.set( 'port', ( process.env.PORT || 8000 ) );
 
 // configure middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, '../')));
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( express.static( path.join( __dirname, '../' ) ) );
 
 // configure server
-var server = http.createServer(app);
-server.listen( app.get('port'), function() {
-	console.log( `Server is now listening on ${app.get('port')}` );
+var server = http.createServer( app );
+server.listen( app.get( 'port' ), function() {
+	console.log( `Server is now listening on port: ${ app.get( 'port' ) }` );
 } );
 
 module.exports = app;
