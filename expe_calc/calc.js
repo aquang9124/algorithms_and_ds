bb6a209983db230577cf3276e1a49724de65440f
Alex Quang <alexquang1991@gmail.com>
2016-08-11 12:22:53 -0700

    improved text output for both calcs

--- expe_calc/calc.js
+++ expe_calc/calc.js
@@ -28,7 +28,7 @@ function handleExpClick(e) {
 		getElem( '.exp-sum' ).innerHTML = sum;
 	} else {
 		var expStr = `<p id="exp-total">Your total monthly expenses will be: <span class="exp-sum">${sum}</span></p>`;
-		getElem( '.calc-container' ).insertAdjacentHTML( 'beforeend', expStr );
+		getElem( '#exp-calc' ).insertAdjacentHTML( 'beforeend', expStr );
 	}
 
 	// get remaining money for the month and append html to calc container
@@ -38,7 +38,7 @@ function handleExpClick(e) {
 		getElem( '#rem-total span' ).innerHTML = monthlySum;
 	} else {
 		var remStr = `<p id="rem-total">You will have <span>${monthlySum}</span> left over each month</p>`;
-		getElem( '.calc-container' ).insertAdjacentHTML( 'beforeend', remStr );
+		getElem( '#exp-calc' ).insertAdjacentHTML( 'beforeend', remStr );
 	}
 	
 }
@@ -51,7 +51,7 @@ function handleFareClick(e) {
 	let baseURI = `http://api.bart.gov/api/sched.aspx?cmd=fare&orig=${origin}&dest=${destination}&date=today&key=${bartKey}`;
 
 	let request = new XMLHttpRequest();
-	
+
 	request.open('GET', baseURI, true);
 
 	request.onload = function() {
@@ -59,17 +59,30 @@ function handleFareClick(e) {
 		// Success!
 			let xml = request.responseXML;
 			let fare = xml.getElementsByTagName("fare")[0].innerHTML;
+			let roundTripTotal = (parseFloat(fare) * 2).toFixed(2);
+			let monthlyTransCost = parseFloat(roundTripTotal) * 20;
 			
-			if ( getElem( '#fare-total' ) ) {
-				getElem( '#fare-total span' ).innerHTML = fare;
+			if ( getElem( '#fare-total' ) && getElem( '#round-trip' ) ) {
+
+				getElem( '#fare-total span' ).innerHTML = `$${fare}`;
+				getElem( '#round-trip span' ).innerHTML = `$${roundTripTotal}`;
+				getElem( '#trans-monthly span' ).innerHTML = `$${monthlyTransCost}`;
+
 			} else {
-				let fareStr = `<p id="fare-total">Your total fare for this trip will be <span>${fare}</span></p>`;
+
+				let fareStr = `<p id="fare-total">Your total fare for this trip will be <span>$${fare}</span></p>`;
+				let rtStr = `<p id="round-trip">For a round trip your fare will be: <span>$${roundTripTotal}</span></p>`;
+				let tStr = `<p id="trans-monthly">In a month you will spend roughly: <span>$${monthlyTransCost}</span> to commute between these locations 5 days a week.</p>`;
+				
 				getElem( '#fare-calc' ).insertAdjacentHTML( 'beforeend', fareStr );
+				getElem( '#fare-calc' ).insertAdjacentHTML( 'beforeend', rtStr );
+				getElem( '#fare-calc' ).insertAdjacentHTML( 'beforeend', tStr );
+
 			}
 			
 		} else {
 		// We reached our target server, but it returned an error
-			console.error( new Error( `Reached server, but it returned ${request.status}`) );
+			console.error( new Error( `Reached server, but it returned ${request.status}` ) );
 		}
 	};
 
