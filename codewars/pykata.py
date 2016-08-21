f887bd2fb1036003c7479b4aabe4a2d25e96ae29
Alex Quang <alexquang1991@gmail.com>
2016-08-20 22:38:35 -0700

    python

--- codewars/pykata.py
+++ codewars/pykata.py
@@ -193,3 +193,16 @@ def word_search(query, seq):
 			results.append(seq[i])
 
 	return results if len(results) > 0 else None
+
+def reverser(s):
+	s = list(s)
+	left = 0
+	right = len(s) - 1
+
+	while left <= right:
+		s[left], s[right] = s[right], s[left]
+
+		left += 1
+		right -= 1
+
+	return ''.join(s)
