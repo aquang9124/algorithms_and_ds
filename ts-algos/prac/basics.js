2eee900db334f6dfcf4141a4417ac3c332aca1e6
Alex Quang <alexquang1991@gmail.com>
2016-08-23 22:12:13 -0700

    fixed issue with boxes not looping

--- ts-algos/prac/basics.js
+++ ts-algos/prac/basics.js
@@ -3,29 +3,26 @@ btn.addEventListener( 'click', (e) => {
 
 	if ( $('#the-only-box').attr('data-fancy') ) 
 	{
-		console.log('It happened!');
-
-		var currentBox = parseInt( $( '#the-only-box' ).attr('data-curr-box') );
-		var boxClasses = document.getElementById( 'the-only-box' ).className.split(/\s+/);
+		var currentBox = parseInt( $( '#the-only-box' ).attr( 'data-curr-box' ) );
+		var boxClasses = document.getElementById( 'the-only-box' ).className.split( /\s+/ );
 
 		if ( boxClasses.length > 1 && currentBox > 0 ) 
 		{
 			$( '#the-only-box' ).removeClass( `box${ currentBox }` );
-			console.log(currentBox, boxClasses);
+
 			if ( currentBox < 6 ) {
 				currentBox += 1;
 			} else {
 				currentBox = 1;
 			}
-
-			$( '#the-only-box' ).addClass( `box${ currentBox }` );
 		}
 		else
 		{
 			currentBox += 1;
-			$( '#the-only-box' ).attr( 'data-curr-box', currentBox);
-			$( '#the-only-box' ).addClass( `box${ currentBox }` );
 		}
+
+		$( '#the-only-box' ).attr( 'data-curr-box', currentBox);
+		$( '#the-only-box' ).addClass( `box${ currentBox }` );
 
 	}
 	else
