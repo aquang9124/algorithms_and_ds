7bb5f445c7cd495d154eb667a173f1f80ca909e0
Alex Quang <alexquang1991@gmail.com>
2016-08-23 22:08:12 -0700

    boxes are switching colors, still buggy

--- ts-algos/prac/basics.js
+++ ts-algos/prac/basics.js
@@ -1,10 +1,32 @@
 var btn = document.getElementById('clicka');
 btn.addEventListener( 'click', (e) => {
 
-	if ( $('.box').attr('data-fancy') ) 
+	if ( $('#the-only-box').attr('data-fancy') ) 
 	{
 		console.log('It happened!');
-		$('.box').hasClass('box1') ? false : $('.box').addClass('box1');
+
+		var currentBox = parseInt( $( '#the-only-box' ).attr('data-curr-box') );
+		var boxClasses = document.getElementById( 'the-only-box' ).className.split(/\s+/);
+
+		if ( boxClasses.length > 1 && currentBox > 0 ) 
+		{
+			$( '#the-only-box' ).removeClass( `box${ currentBox }` );
+			console.log(currentBox, boxClasses);
+			if ( currentBox < 6 ) {
+				currentBox += 1;
+			} else {
+				currentBox = 1;
+			}
+
+			$( '#the-only-box' ).addClass( `box${ currentBox }` );
+		}
+		else
+		{
+			currentBox += 1;
+			$( '#the-only-box' ).attr( 'data-curr-box', currentBox);
+			$( '#the-only-box' ).addClass( `box${ currentBox }` );
+		}
+
 	}
 	else
 	{
