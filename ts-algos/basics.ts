2ec285e6b0e7d745f8369d0a182a87fa5d6cde9a
Alex Quang <alexquang1991@gmail.com>
2016-08-27 14:33:14 -0700

    ts algos

--- ts-algos/basics.ts
+++ ts-algos/basics.ts
@@ -36,3 +36,69 @@ function calcProfits(gain: number, loss: number, time: number): number {
 
 	return sum;
 }
+
+// print 1 to a number N
+function print1ToN(n: number): void {
+	for (let i: number = 1; i < n; i++) {
+		console.log(i);
+	}
+}
+
+// recursively print 1 to N
+function rPrinter(n: number, i: number = 0): void {
+	if (i === n) {
+		return;
+	}
+
+	console.log(i);
+	rPrinter(n, i + 1);
+}
+
+// remove all negatives from array
+function removeNegatives(arr: number[]): any {
+	let numNegs: number = 0;
+	let i: number;
+
+	for (i = 0; i < arr.length; i++)
+	{
+		if (arr[i] >= 0) 
+		{
+			arr[i - numNegs] = arr[i];
+		}
+		else
+		{
+			numNegs++;
+		} 
+	}
+
+	arr.length -= numNegs;
+	return arr;
+}
+
+interface Person {
+	name: string,
+	age: number,
+	hobbies: string[]
+}
+
+function printPerson(person: Person) {
+	let result: string = "";
+
+	person.hobbies.forEach((hobby: string, i: number) => {
+		result += hobby;
+
+		if (hobby !== person.hobbies[person.hobbies.length - 1]) {
+			result += ", ";
+		} else {
+			result += ".";
+		}
+	});
+
+	console.log(`${person.name} is ${person.age} years old and likes ${result}`);
+}
+
+let myPerson = {
+	name: "Alex",
+	age: 24,
+	hobbies: ["programming", "eating", "playing games"]
+};
