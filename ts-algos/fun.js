bcd5e4bcacb3072472508a0825bb32bf337ea932
Alex Quang <alexquang1991@gmail.com>
2016-08-28 22:12:14 -0700

    algos

---
+++ ts-algos/fun.js
@@ -0,0 +1,5 @@
+console.log('Hello there'); // 1
+setTimeout(function () {
+    console.log('Hello now'); // 3
+}, 0);
+console.log('Hello guys'); // 2
---
+++ ts-algos/fun.ts
@@ -0,0 +1,7 @@
+console.log('Hello there'); // 1
+
+setTimeout(() => {
+    console.log('Hello now'); // 3
+}, 0);
+
+console.log('Hello guys'); // 2
