371e5f3ef4f7bc19dd982a0e51b00b4f31aa3096
Alex Quang <alexquang1991@gmail.com>
2016-08-29 21:11:32 -0700

    fun with ts

--- ts-algos/fun.js
+++ ts-algos/fun.js
@@ -1,5 +1,23 @@
+/*
+// Some fun with console.log and event loop
 console.log('Hello there'); // 1
-setTimeout(function () {
+
+setTimeout(() => {
     console.log('Hello now'); // 3
 }, 0);
+
 console.log('Hello guys'); // 2
+*/
+// codewars Hit Count algorithm challenge
+function counterEff(hitCount) {
+    var i;
+    var result = [[], [], [], []];
+    for (i = 0; i < 4; i++) {
+        var digit = parseInt(hitCount[i]);
+        for (var j = 0; j <= digit; j++) {
+            result[i].push(j);
+        }
+    }
+    return result;
+}
+console.log(counterEff("1250"));
--- ts-algos/fun.ts
+++ ts-algos/fun.ts
@@ -1,3 +1,5 @@
+/* 
+// Some fun with console.log and event loop
 console.log('Hello there'); // 1
 
 setTimeout(() => {
@@ -5,3 +7,20 @@ setTimeout(() => {
 }, 0);
 
 console.log('Hello guys'); // 2
+*/
+
+// codewars Hit Count algorithm challenge
+function counterEff(hitCount: string) : number[][] {
+    let i : number;
+    let result : number[][] = [[], [], [], []];
+
+    for (i = 0; i < 4; i++) {
+        let digit : number = parseInt(hitCount[i]);
+
+        for (let j : number = 0; j <= digit; j++) {
+            result[i].push(j);
+        }
+    }
+
+    return result;
+}
