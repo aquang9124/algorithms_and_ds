06f9589542ced6c1e71a0ace9e395a78dd0b632c
Alex Quang <alexquang1991@gmail.com>
2016-09-13 21:30:35 -0700

    ng fun

--- super_simple_ng/app/infocard.controller.js
+++ super_simple_ng/app/infocard.controller.js
@@ -6,7 +6,25 @@
         var vm = this;
 
         vm.user = {
-            name: "Alex Quang"
+            name: "Alex Quang",
+            address: {
+                street: 'Horizon Court',
+                city: 'Imperial Capital',
+                planet: 'Aqua'
+            },
+            friends: [
+                'Xuan',
+                'Quang',
+                'Trung'
+            ],
+            rank: null
         };
+        vm.knightMe = knightMe;
+
+        // implementations
+        function knightMe(rank) {
+            vm.user.rank = vm.user.rank ? null : rank;
+            return rank;
+        }
     }
 })();
