c7b5f577cc8a79d3ecadf840073d9c58b43361d3
Alex Quang <alexquang1991@gmail.com>
2016-09-22 22:04:38 -0700

    ts fun

--- ts-algos/codewars/prac-one.js
+++ ts-algos/codewars/prac-one.js
@@ -20,3 +20,30 @@ function moveZeroes(arr) {
     }
     return result;
 }
+function findMax(arr) {
+    var max = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+function bubbleSort(arr) {
+    var limit = 1;
+    var swapped = true;
+    while (swapped) {
+        swapped = false;
+        for (var i = 0; i < arr.length - limit; i++) {
+            if (arr[i] > arr[i + 1]) {
+                var temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+        limit--;
+    }
+    return arr;
+}
+console.log(bubbleSort([0, -1, -55, 23, 9, 5, 10]));
--- ts-algos/codewars/prac-one.ts
+++ ts-algos/codewars/prac-one.ts
@@ -25,3 +25,39 @@ function moveZeroes(arr: Array<any>): Array<any> {
 
     return result;
 }
+
+function findMax(arr: Array<number>): number {
+    let max = arr[0];
+
+    for(let i: number = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+function bubbleSort(arr: number[]): number[] {
+    let limit = 1;
+    let swapped = true;
+
+    while (swapped) {
+        swapped = false;
+
+        for (let i: number = 0; i < arr.length - limit; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+
+        limit--;
+    }
+
+    return arr;
+}
+
+console.log(bubbleSort([0, -1, -55, 23, 9, 5, 10]));
