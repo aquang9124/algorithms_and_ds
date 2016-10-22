b8009b591c32490634fe2e977277edac48f48edf
Alex Quang <alexquang1991@gmail.com>
2016-10-22 13:48:04 -0700

    algos

--- ts-algos/prac/easy.js
+++ ts-algos/prac/easy.js
@@ -18,4 +18,56 @@ function rCountToX(x) {
     doCount();
     return result;
 }
-console.log(rCountToX(20));
+function flattenArr(arr) {
+    var result = [];
+    function makeFlat(item) {
+        if (!Array.isArray(item)) {
+            result.push(item);
+        }
+        else {
+            for (var i = 0; i < item.length; i++) {
+                makeFlat(item[i]);
+            }
+        }
+    }
+    makeFlat(arr);
+    return result;
+}
+function climbStairs(steps) {
+    if (steps === 0) {
+        return 1;
+    }
+    else if (steps < 0) {
+        return 0;
+    }
+    return climbStairs(steps - 1) + climbStairs(steps - 2);
+}
+var SllNode = (function () {
+    function SllNode(data) {
+        this.data = data;
+    }
+    return SllNode;
+}());
+var SinglyLinkedList = (function () {
+    function SinglyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.count = 0;
+    }
+    SinglyLinkedList.prototype.add = function (data) {
+        var newNode = new SllNode(data);
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            this.tail.next = newNode;
+        }
+        this.count += 1;
+        return this;
+    };
+    return SinglyLinkedList;
+}());
+var sll = new SinglyLinkedList();
+sll.add(10);
+console.log(sll);
--- ts-algos/prac/easy.ts
+++ ts-algos/prac/easy.ts
@@ -25,4 +25,65 @@ function rCountToX(x: number): number[] {
     return result;
 }
 
-console.log(rCountToX(20));
+function flattenArr(arr: number[][]): number[] {
+    let result: number[] = [];
+
+    function makeFlat(item) {
+        if (!Array.isArray(item)) {
+            result.push(item);
+        } else {
+            for (var i = 0; i < item.length; i++) {
+                makeFlat(item[i]);
+            }
+        }
+    }
+
+    makeFlat(arr);
+
+    return result;
+}
+
+function climbStairs(steps: number) {
+    if (steps === 0) {
+        return 1;
+    } else if (steps < 0) {
+        return 0;
+    }
+
+    return climbStairs(steps - 1) + climbStairs(steps - 2);
+}
+
+class SllNode {
+    data: number;
+    next: SllNode;
+
+    constructor(data: number) {
+        this.data = data;
+    }
+}
+
+class SinglyLinkedList {
+    head: SllNode;
+    tail: SllNode;
+    count: number;
+
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.count = 0;
+    }
+
+    add(data: number) {
+        let newNode = new SllNode(data);
+
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode; 
+        } else {
+            this.tail.next = newNode;
+        }
+
+        this.count += 1;
+        return this;
+    }
+}
